87b6e3f40168bab9b3200316cd527b68b010ae37
Stackbit Code Editor <projects@stackbit.com>
2021-05-17 21:18:53 +0000

    config.js: changed

--- config.js
+++ config.js
@@ -9,7 +9,7 @@ module.exports = {
   manifestIcon: 'src/assets/img/website-icon.png',
   pathPrefix: `/gatsby-starter-spectral/`, // This path is subpath of your hosting https://domain/portfolio
   heading: 'WARSZTAT RPG',
-  subHeading: 'PASJA - ZABAWA - ',
+  subHeading: 'PASJA - ZABAWA - PRACA',
   // social
   socialLinks: [
     {
